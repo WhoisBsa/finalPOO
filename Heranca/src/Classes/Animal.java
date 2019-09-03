a924e8c52ccd900d1b2c7056875eb5bf9c9fd1d9
HLAM <hemilioaraujo@gmail.com>
2019-09-02 22:08:30 -0300

    removido comentários desnecessários

--- Heranca/src/Classes/Animal.java
+++ Heranca/src/Classes/Animal.java
@@ -22,58 +22,35 @@ public class Animal {
         System.out.println("Animal Movimentando!");
     }
 
-    /**
-     * @return the cor
-     */
+
     public String getCor() {
         return cor;
     }
 
-    /**
-     * @param cor the cor to set
-     */
     public void setCor(String cor) {
         this.cor = cor;
     }
 
-    /**
-     * @return the peso
-     */
     public double getPeso() {
         return peso;
     }
 
-    /**
-     * @param peso the peso to set
-     */
     public void setPeso(double peso) {
         this.peso = peso;
     }
 
-    /**
-     * @return the olhos
-     */
     public String getOlhos() {
         return olhos;
     }
 
-    /**
-     * @param olhos the olhos to set
-     */
     public void setOlhos(String olhos) {
         this.olhos = olhos;
     }
 
-    /**
-     * @return the tamanho
-     */
     public double getTamanho() {
         return tamanho;
     }
 
-    /**
-     * @param tamanho the tamanho to set
-     */
     public void setTamanho(double tamanho) {
         this.tamanho = tamanho;
     }
