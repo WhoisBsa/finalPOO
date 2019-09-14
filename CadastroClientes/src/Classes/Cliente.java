668f68bcc67f74d54fd7043fb980b1c22dcb64df
HLAM <hemilioaraujo@gmail.com>
2019-09-13 21:54:11 -0300

    adicionadas as classes Usuario e Cliente

---
+++ CadastroClientes/src/Classes/Cliente.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+public class Cliente {
+    
+    private String nome;
+    private String cpf;
+    private String endereco;
+    
+    public Cliente(){
+        
+    }
+    
+    public Cliente(String nome,String cpf, String endereco){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+}
