e03b28325ff7b20377173d0ef7e4a499115e71cc
HLAM <hemilioaraujo@gmail.com>
2019-09-18 22:45:27 -0300

    adicionadas instancias das janelas

---
+++ ShowDoMilhao/src/Classes/Jogador.java
@@ -0,0 +1,34 @@
+
+package Classes;
+
+
+public class Jogador {
+    private String nome;
+    private int pontos = 0;
+    
+    public Jogador(){
+        
+    }
+    
+    public Jogador(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+    
+    
+}
