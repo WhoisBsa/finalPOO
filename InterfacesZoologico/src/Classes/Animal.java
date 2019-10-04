50b7ce9e5f086f47c2605eadcff7d57a21bc169a
HLAM <hemilioaraujo@gmail.com>
2019-10-04 07:35:29 -0300

    iniciado exercício de interfaces exemplo zoologico

---
+++ InterfacesZoologico/src/Classes/Animal.java
@@ -0,0 +1,41 @@
+package Classes;
+
+public abstract class Animal {
+    private double peso;
+    private String cor;
+    private String tamanho;
+    
+    public void comer(){
+        
+    }
+    
+    public void dormir(){
+        
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+    
+    
+}
---
+++ InterfacesZoologico/src/interfaceszoologico/InterfacesZoologico.java
@@ -0,0 +1,9 @@
+package interfaceszoologico;
+
+public class InterfacesZoologico {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
