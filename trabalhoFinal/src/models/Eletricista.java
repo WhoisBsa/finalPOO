3e9a3ef54568be6bc1b329796641191fa37e3ddc
HLAM <hemilioaraujo@gmail.com>
2019-11-20 20:31:59 -0300

    atualizados arquivos

--- trabalhoFinal/src/models/Eletricista.java
+++ trabalhoFinal/src/models/Eletricista.java
@@ -6,5 +6,24 @@ package models;
  * @author hemiliom
  */
 public class Eletricista extends Funcionario {
+    private String funcao;
+
+    public Eletricista() {
+        
+    }
+
+    public Eletricista(String nome, String matricula, String turno, String funcao) {
+        super(nome, matricula, turno);
+        this.funcao = funcao;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+    
     
 }
