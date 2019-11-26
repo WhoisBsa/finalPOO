d8d98e61f4f3e6275bd7b1cd5418da2f7e180c17
HLAM <hemilioaraujo@gmail.com>
2019-11-26 00:19:23 -0200

    Instanciando a view login.

--- src/trabalhofinal/TrabalhoFinal.java
+++ src/trabalhofinal/TrabalhoFinal.java
@@ -1,26 +1,12 @@
-
 package trabalhofinal;
 
-import models.Eletricista;
-import models.Funcionario;
-import models.Mecanico;
-import models.Relatorio;
+import views.Login;
 
 public class TrabalhoFinal {
 
     public static void main(String[] args) {
-        Mecanico m = new Mecanico();
-        Eletricista e = new Eletricista();
-        Relatorio r = new Relatorio();
-        
-        r.setCodMaterial("BUNDA");
-        
-        System.out.println(r.getCodMaterial());
-        
-        e.setNome("Manoel");
-        m.setNome("Joaquim");
-        System.out.println(m.getNome());
-        System.out.println(e.getNome());
+        Login l = new Login();
+        l.setVisible(true);
     }
     
 }
